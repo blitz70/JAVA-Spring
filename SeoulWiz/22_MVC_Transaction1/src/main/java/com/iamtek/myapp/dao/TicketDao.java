c261d942f36ad33c764634b2a803e1e8f5332f07
blitz-out <blitz70@hanmail.net>
2016-09-27 02:48:12 +0900

    MVC Transaction update

--- SeoulWiz/22_MVC_Transaction1/src/main/java/com/iamtek/myapp/dao/TicketDao.java
+++ SeoulWiz/22_MVC_Transaction1/src/main/java/com/iamtek/myapp/dao/TicketDao.java
@@ -6,6 +6,10 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.iamtek.myapp.dto.TicketDto;
 
@@ -13,44 +17,57 @@ public class TicketDao {
 
 	private String sql;
 	private JdbcTemplate template;
+	private PlatformTransactionManager transactionManager;
 	
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
+	}
 	
 	public TicketDao() {
-		System.out.println("TicketDao()");
-		System.out.println(template);
+		System.out.println("TicketDao() :" + template);
 	}
 	
 	public void buyTicket(TicketDto dto) {
-		System.out.println("TicketDao.buyTicket()");
+		
 		final String consumerId = dto.getConsumerId();
 		final String amount = dto.getAmount();
-		System.out.println("consumerId:"+consumerId);
-		System.out.println("amount:"+amount);
 		
-		template.update(new PreparedStatementCreator() {
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				sql = "INSERT INTO card (consumerId, amount) VALUES (?, ?)";
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setString(1, consumerId);
-				ps.setString(2, amount);
-				return ps;
-			}
-		});
+		TransactionDefinition definition = new DefaultTransactionDefinition();
+		TransactionStatus status = transactionManager.getTransaction(definition);
 		
-		template.update(new PreparedStatementCreator() {
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				sql = "INSERT INTO ticket (consumerId, countNum) VALUES (?, ?)";
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setString(1, consumerId);
-				ps.setString(2, amount);
-				return ps;
-			}
-		});
+		try {
+			//logic1
+			template.update(new PreparedStatementCreator() {
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+					sql = "INSERT INTO card (consumerId, amount) VALUES (?, ?)";
+					PreparedStatement ps = con.prepareStatement(sql);
+					ps.setString(1, consumerId);
+					ps.setString(2, amount);
+					return ps;
+				}
+			});
+			//logic2
+			template.update(new PreparedStatementCreator() {
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+					sql = "INSERT INTO ticket (consumerId, countNum) VALUES (?, ?)";
+					PreparedStatement ps = con.prepareStatement(sql);
+					ps.setString(1, consumerId);
+					ps.setString(2, amount);
+					return ps;
+				}
+			});
+			transactionManager.commit(status);
+		} catch (Exception e) {
+			System.out.println("buyTicket()");
+			transactionManager.rollback(status);;
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
